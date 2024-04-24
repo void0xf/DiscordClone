9939184abd94324e5aca05350644d333960e784a
void0xf <150953141+void0xf@users.noreply.github.com>
2024-04-24 13:47:56 +0200

    chore: vitest, testing-library added to project

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import svgrPlugin from 'vite-plugin-svgr';
+/// <reference types="vitest"/>
+/// <reference types="vite/client"/>
 
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgrPlugin from "vite-plugin-svgr";
 
 export default defineConfig({
   plugins: [react(), svgrPlugin({})],
-})
+  test: {
+    globals: true,
+    environment: "jsdom",
+    css: true,
+    setupFiles: "./test/setup.ts",
+  },
+});
