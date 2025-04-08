48a92aa6e8eb3347b437225ac62539b668af8b05
void0xf <lucyfer99100@gmail.com>
2025-04-08 21:34:31 +0200

    chore: remove react icons

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   swcMinify: true,
+  experimental: {
+    webpackBuildWorker: true,
+    turbo: {},
+  },
 };
 
 export default nextConfig;
